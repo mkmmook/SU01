eaa33d1fc60a1c05ca6faecc5384d2640daea232
pCm <pcm@pCms-MacBook-Air.local>
2022-02-16 13:26:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
-import React from 'react';
-import SlideBar from './SlideBar';
-import Form from './Form';
-import './dropdown.css';
+import "./App.css";
+import React from "react";
+import Form from "./component/Form";
+import "./dropdown.css";
+import Predict from "./component/Predict";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <SlideBar />
-        <Form />
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Form />
+          </Route>
+          <Route path="/Predict" exact>
+            <Predict />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/component/Predict.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useHistory } from 'react-router-dom';
+
+const Predict = (props) => {
+  const history = useHistory();
+  return (
+    <header className="Body">
+      <h1>helo </h1>
+    </header>
+  );
+};
+
+export default Predict;
